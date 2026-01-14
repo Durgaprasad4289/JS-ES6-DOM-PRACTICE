54bffed464d9e2eee49f92386c9f481c3db5309f
Durgaprasad4289 <durgaprasad04289@gmail.com>
2026-01-14 17:50:03 +0530

    Update script.js

--- project-2/script.js
+++ project-2/script.js
@@ -1,16 +1,20 @@
-// ------------------------ PROJECT - 2 --------------------------------
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let result = document.getElementById("result");
 
-let weight=document.getElementById("weight");
-let height=document.getElementById("height");
-let result=document.getElementById("result");
-document.getElementById("calculateBtn").addEventListener("click",e=>{
-    let bmi=weight.value/Math.pow(height.value,2);
-    result.innerText=bmi.toFixed(2);
-    
+document.getElementById("calculateBtn").addEventListener("click", () => {
+
+    if (weight.value === "" || height.value === "") {
+        result.innerText = "Please enter values";
+        return;
+    }
+
+    let bmi = weight.value / Math.pow(height.value / 100, 2);
+    result.innerText = bmi.toFixed(2);
 });
 
-document.getElementById("reset").addEventListener('click',e=>{
-    weight.value='';
-    height.value='';
-    result.innerText='RESULT';
-})
+document.getElementById("reset").addEventListener("click", () => {
+    weight.value = "";
+    height.value = "";
+    result.innerText = "RESULT";
+});
