256779e712415142c1f32e1b0adfe31482e43f73
Durgaprasad4289 <durgaprasad04289@gmail.com>
2026-01-15 08:40:40 +0530

    Update script.js

--- project-2/script.js
+++ project-2/script.js
@@ -1,20 +1,22 @@
-let weight = document.getElementById("weight").value;
-let height = document.getElementById("height").value;
+let weightInput = document.getElementById("weight");
+let heightInput = document.getElementById("height");
 let result = document.getElementById("result");
 
 document.getElementById("calculateBtn").addEventListener("click", () => {
+    let weight = parseFloat(weightInput.value);
+    let height = parseFloat(heightInput.value);
 
-    if (weight.value === "" || height.value === "") {
-        result.innerText = "Please enter values";
+    if (isNaN(weight) || isNaN(height) || height <= 0) {
+        result.innerText = "Please enter valid values";
         return;
     }
 
-    let bmi = weight/height*height ;
+    let bmi = weight / (height * height);
     result.innerText = bmi.toFixed(2);
 });
 
 document.getElementById("reset").addEventListener("click", () => {
-    weight.value = "";
-    height.value = "";
+    weightInput.value = "";
+    heightInput.value = "";
     result.innerText = "RESULT";
 });
